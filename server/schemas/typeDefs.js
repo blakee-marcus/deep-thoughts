3a0f2992fa83fa137f0937bfc33a0593aa6a4440
bm <marcusb733@gmail.com>
2022-08-08 01:29:59 -0700

    Add JWT

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type Thought {
@@ -24,12 +24,23 @@ const typeDefs = gql`
         thoughts: [Thought]
         friends: [User]
     }
+    type Auth {
+        token: ID!
+        user: User
+    }
     type Query {
+        me: User
         users: [User]
         user(username: String): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
     }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
 `;
 
 module.exports = typeDefs;
