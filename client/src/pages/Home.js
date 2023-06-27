0a3c338b9f67099da9e82e1568a041842900e52a
bm <marcusb733@gmail.com>
2023-06-26 19:19:04 -0700

    Deploy heroku

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,6 +14,9 @@ const Home = () => {
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const thoughts = data?.thoughts || [];
   const loggedIn = Auth.loggedIn();
+  console.log('home.js');
+  console.log(userData);
+
   return (
     <section className='flex-row justify-space-between overflow-scroll max-100-vh border-right'>
       {loggedIn && (
@@ -24,15 +27,7 @@ const Home = () => {
       )}
       <div className='w-100 mb-3 col-lg-12'>
         {loading ? <div>Loading...</div> : <ThoughtList thoughts={thoughts} />}
-        {loggedIn && userData ? (
-          <div className='col-12 col-lg-3 mb-3'>
-            {/* <FriendList
-              username={userData.me.username}
-              friendCount={userData.me.friendCount}
-              friends={userData.me.friends}
-            /> */}
-          </div>
-        ) : null}
+        
       </div>
     </section>
   );
